820a251fcbab73d5fac72ff68fce9a2b06a39b2d
Ndean0103 <nikolaspro31@gmail.com>
2023-03-27 16:17:25 -0700

    Added stepper control functions

---
+++ Source Code/loadcell.h
@@ -0,0 +1,20 @@
+/* loadcell.h
+ * Nikolas Prochaska
+ * For ColorSmart SFR
+ * Definitions and constants for the operation of 
+ * the loadcells used to weigh the material hoppers
+ */
+
+#ifndef loadcellh
+#define loadcellh
+
+#ifndef pins
+#include "pins.h"
+#endif   // pins
+
+#ifndef HX711H
+#include<HX711.h>
+#define HX711H
+#endif   // 
+
+#endif   // loadcellh
---
+++ Source Code/pins.h
@@ -0,0 +1,47 @@
+#ifndef pins
+    #define pins
+
+    #define h1_STEP 28
+    #define h1_DIR  29
+
+    #define h2_STEP 14
+    #define h2_DIR  15
+
+    #define h3_STEP 16
+    #define h3_DIR  17
+
+    #define h4_STEP 18
+    #define h4_DIR  19
+
+    #define h5_STEP 20
+    #define h5_DIR  21
+
+    #define h6_STEP 22
+    #define h6_DIR  23
+
+    #define ALIGN_STEP_PIN 41
+    #define ALIGN_DIR_PIN 39
+    #define ALIGN_EN_PIN 38
+    #define AL_CS_PIN 42
+    #define AL_SW_MOSI 34
+    #define AL_SW_MISO 35
+    #define AL_SW_SCK 32
+    #define AL_DIAG1 25
+
+    #define PULL_STEP_PIN 41
+    #define PULL_DIR_PIN 39
+    #define PULL_EN_PIN 38
+    #define PU_CS_PIN 42
+    #define PU_SW_MOSI 34
+    #define PU_SW_MISO 35
+    #define PU_SW_SCK 32
+
+    #define WIND_STEP_PIN 41
+    #define WIND_DIR_PIN 39
+    #define WIND_EN_PIN 38
+    #define W_CS_PIN 42
+    #define W_SW_MOSI 34
+    #define W_SW_MISO 35
+    #define W_SW_SCK 32
+
+#endif
