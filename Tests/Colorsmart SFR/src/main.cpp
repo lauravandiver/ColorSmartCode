8c0024ec52a8723ae44e24f759fc6c811660f687
Ndean0103 <nikolaspro31@gmail.com>
2023-03-28 15:39:29 -0700

    File changes

--- Tests/Colorsmart SFR/src/main.cpp
+++ Tests/Colorsmart SFR/src/main.cpp
@@ -1,17 +1,9 @@
-#include"puller.h"
+#include <Arduino.h>
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
-  while(!Serial);
- 
-  pullerInit();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  pullerRunSpeed(1600);
-
-  delay(10);
-
 }
