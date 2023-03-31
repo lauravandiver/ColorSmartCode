31d75b49a07c52784ea1da10f3503d95db40110f
Ndean0103 <nikolaspro31@gmail.com>
2023-03-31 12:44:34 -0700

    Moved loadcell tests to previous

---
+++ src/Previous/manual.ino
@@ -0,0 +1,61 @@
+#include <Arduino.h>
+#include"aligner.h"
+#include"hopperStepper.h"
+#include"loadcell.h"
+#include"puller.h"
+#include"digitalIndicator.h"
+
+void setup() {
+  Serial.begin(115200);
+  while(!Serial);
+  Serial.println("Begin");
+  Serial.println("Press r to read or t to tare or d to read diameter");
+  Serial.println();
+
+  loadcellInit();
+  indicatorInit();
+}
+
+int weight;
+
+void loop() {
+  delay(500);
+
+  if(Serial.available()){
+    char temp = Serial.read();
+    Serial.println(temp);
+    Serial.println();
+
+    if(temp == 'r'){
+      weight = readLoadCell(1);
+      Serial.print("Reading: ");
+      Serial.print(weight);
+      Serial.print(" grams");
+      Serial.println();
+      Serial.println("Press r to read or t to tare");
+    }
+
+    else if(temp == 't'){
+      Serial.print("Previous Zero Factor: ");
+      Serial.print(zero_factor);
+      Serial.println();
+      //Serial.println(millis());
+
+      loadcellTare();
+      
+      //Serial.println(millis());
+      Serial.print("New Zero Factor: ");
+      Serial.print(zero_factor);
+      Serial.println();
+      Serial.println("Press r to read or t to tare");
+    }
+
+    else if(temp == 'd'){
+        Serial.print("Diameter Reading: ");
+        Serial.print(indicatorReadMM(), 3);
+        Serial.println();
+        Serial.println("Press r to read or t to tare or d to read diameter");
+    }
+  }
+
+}
