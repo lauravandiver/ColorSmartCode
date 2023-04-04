be745287eb888ea9fdeca2bd885b480c4e6c5b30
morandon <morandon@my.erau.edu>
2023-04-03 17:08:20 -0700

    temp control updates

--- include/tempContol.h
+++ include/tempContol.h
@@ -1,14 +1,14 @@
+# pragma once
 // Set up thermocouples
 
-#include "max6675.h"
+#include "MAX6675.h"
 
-#define thermoDO1 = 49;
-#define thermoDO2 = 50;
-#define thermoDO3 = 51;
-#define thermoCLK = 52;
-#define thermoCS = 53;
-
-#define done = 0;
+#define thermoDO1 49
+#define thermoDO2 50
+#define thermoDO3 51
+#define thermoCLK 52
+#define thermoCS 53
+#define done = 0
 
 // set up thermocouples
 MAX6675 thermocouple1(thermoCLK, thermoCS, thermoDO1);
@@ -17,10 +17,10 @@ MAX6675 thermocouple3(thermoCLK, thermoCS, thermoDO3);
 
 // Set up heaterband relays
 
-#define relay_1 = 23;   // heaterband 1
-#define relay_2 = 25;   // heaterband 2
-#define relay_3 = 27;   // heaterband 3
-#define relay_4 = 29;   // fans
+#define relay_1 = 23   // heaterband 1
+#define relay_2 = 25   // heaterband 2
+#define relay_3 = 27   // heaterband 3
+#define relay_4 = 29   // fans
 
 int gettemp1() {
   return thermocouple1.readCelsius();
@@ -34,13 +34,13 @@ int gettemp3() {
 }
 
 void startExtruder(int destemp) {
-  static destemp1 = destemp;
-  static destemp2 = destemp1 - 40;
-  static destemp3 = destemp2 - 60;
+  static int destemp1 = destemp;
+  static int destemp2 = destemp1 - 40;
+  static int destemp3 = destemp2 - 60;
 
-  static overshoot1 = 20;
-  static overshoot2 = 40;
-  static overshoot3 = 60;
+  static int overshoot1 = 20;
+  static int overshoot2 = 40;
+  static int overshoot3 = 60;
 
   while (gettemp1() <(destemp1 - overshoot1)){
     digitalWrite(relay_1, LOW);
@@ -55,11 +55,11 @@ void startExtruder(int destemp) {
 }
 
 void adjusttemps(int destemp){
-  static destemp1 = destemp;
-  static destemp2 = destemp1 - 40;
-  static destemp3 = destemp2 - 60;
+  static int destemp1 = destemp;
+  static int destemp2 = destemp1 - 40;
+  static int destemp3 = destemp2 - 60;
 
-  static undershoot = 1;
+  static int undershoot = 1;
 
   // Adjust temp1
   if (gettemp1() <= (destemp1 + undershoot)){
