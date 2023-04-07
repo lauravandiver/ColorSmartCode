9e93d5c551678c9141b1ec01687ff92430eb8e04
Ndean0103 <nikolaspro31@gmail.com>
2023-04-07 16:24:41 -0700

    Made extruder startup nonblocking

--- include/tempContol.h
+++ include/tempContol.h
@@ -1,60 +1,65 @@
-# pragma once
+#pragma once
 // Set up thermocouples
 
 #include "MAX6675.h"
-
-#define thermoDO1 49
-#define thermoDO2 50
-#define thermoDO3 51
-#define thermoCLK 52
-#define thermoCS 53
-#define done = 0
+#include "pins.h"
 
 // set up thermocouples
 MAX6675 thermocouple1(thermoCLK, thermoCS, thermoDO1);
 MAX6675 thermocouple2(thermoCLK, thermoCS, thermoDO2);
 MAX6675 thermocouple3(thermoCLK, thermoCS, thermoDO3);
 
-// Set up heaterband relays
+int gettemp1() { return thermocouple1.readCelsius(); }
 
-#define relay_1 = 23   // heaterband 1
-#define relay_2 = 25   // heaterband 2
-#define relay_3 = 27   // heaterband 3
-#define relay_4 = 29   // fans
+int gettemp2() { return thermocouple2.readCelsius(); }
+int gettemp3() { return thermocouple3.readCelsius(); }
 
-int gettemp1() {
-  return thermocouple1.readCelsius();
-}
-
-int gettemp2() {
-  return thermocouple2.readCelsius();
-}
-int gettemp3() {
-  return thermocouple3.readCelsius();
-}
-
-void startExtruder(int destemp) {
+bool startExtruder(int destemp) {
   static int destemp1 = destemp;
   static int destemp2 = destemp1 - 40;
   static int destemp3 = destemp2 - 60;
+
+  static bool zone1_done = false;
+  static bool zone2_done = false;
+  static bool zone3_done = false;
 
   static int overshoot1 = 20;
   static int overshoot2 = 40;
   static int overshoot3 = 60;
 
-  while (gettemp1() <(destemp1 - overshoot1)){
+  if (gettemp1() < (destemp1 - overshoot1) && !zone1_done) {
     digitalWrite(relay_1, LOW);
+  } else if (gettemp1() >= (destemp1 - overshoot1)) {
+    zone1_done = true;
   }
-  while (gettemp2() <(destemp2 - overshoot2)){
-    digitalWrite(relay_2, LOW);
-    //checktemps();
-  }
-//  while (gettemp3() <(destemp3 - overshoot3)){
-//    digitalWrite(relay_3, LOW);
-//  }
-}
 
-void adjusttemps(int destemp){
+  if (gettemp2() < (destemp2 - overshoot2) && (zone1_done && !zone2_done)) {
+    digitalWrite(relay_2, LOW);
+    // checktemps();
+  } else if (gettemp2() >= (destemp2 - overshoot2)) {
+    zone2_done = true;
+  }
+
+  if (gettemp3() < (destemp3 - overshoot3) &&
+      (zone1_done && zone2_done && !zone3_done)) {
+    digitalWrite(relay_3, LOW);
+  } else if (gettemp3() >= (destemp3 - overshoot3)) {
+    zone3_done = true;
+  }
+
+  // Determine if start is done. If so reset so it can be called again
+  if (zone1_done && zone2_done && zone3_done) {
+    zone1_done = false;
+    zone2_done = false;
+    zone3_done = false;
+
+    return true;
+  } else
+    return false;
+
+} // startExtruder()
+
+void adjusttemps(int destemp) {
   static int destemp1 = destemp;
   static int destemp2 = destemp1 - 40;
   static int destemp3 = destemp2 - 60;
@@ -62,38 +67,35 @@ void adjusttemps(int destemp){
   static int undershoot = 1;
 
   // Adjust temp1
-  if (gettemp1() <= (destemp1 + undershoot)){
+  if (gettemp1() <= (destemp1 + undershoot)) {
     digitalWrite(relay_1, LOW);
-  }
-  else{
+  } else {
     digitalWrite(relay_1, HIGH);
   }
   // Adjust temp2
-  if (gettemp2() <= (destemp2 + undershoot)){
+  if (gettemp2() <= (destemp2 + undershoot)) {
     digitalWrite(relay_2, LOW);
-  }
-  else{
+  } else {
     digitalWrite(relay_2, HIGH);
   }
   // Adjust temp3
-//  if (gettemp3() <= (destemp3 + undershoot)){
-//    digitalWrite(relay_3, LOW);
-//  }
-//  else{
-//    digitalWrite(relay_3, HIGH);
-//  }
+  //  if (gettemp3() <= (destemp3 + undershoot)){
+  //    digitalWrite(relay_3, LOW);
+  //  }
+  //  else{
+  //    digitalWrite(relay_3, HIGH);
+  //  }
 }
 
-void fanControl(int state){
-  if(state){
+void fanControl(int state) {
+  if (state) {
     digitalWrite(relay_2, LOW);
-  }
-  else{
+  } else {
     digitalWrite(relay_2, HIGH);
   }
 }
 
-void extrudOff(){
+void extrudOff() {
   digitalWrite(relay_1, HIGH);
   digitalWrite(relay_2, HIGH);
   digitalWrite(relay_3, HIGH);
