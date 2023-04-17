11695b85461bd212ae0d974277172fb7209882bb
Ndean0103 <nikolaspro31@gmail.com>
2023-04-16 18:22:06 -0700

    Motor Fixes

--- include/shredder.h
+++ include/shredder.h
@@ -1,31 +1,33 @@
 /* shred.h
  * Devin Cole
  * For ColorSmart SFR
- * Definitions and constants for the operation of 
+ * Definitions and constants for the operation of
  * the shredder system
  */
 
 #pragma once
 
-#include <Arduino.h>
 #include "pins.h"
+#include <Arduino.h>
 
 int pwm_value = 0;
 
 void shredInit() {
-// Initiallize pin modes for the output to the motor driver
-  pinMode(SHRED_DIR,OUTPUT);
-  pinMode(SHRED_PWM,OUTPUT);
+  // Initiallize pin modes for the output to the motor driver
+  pinMode(SHRED_DIR, OUTPUT);
+  pinMode(SHRED_PWM, OUTPUT);
+  pinMode(SHRED_SAFETY, INPUT_PULLUP);
 
-  TCCR3B = TCCR3B & B11111000 | B00000001;
+  // Set PWM on pins 2, 3, 5, to ~31000 Hz
+  TCCR3B = TCCR3B & (B11111000 | B00000001);
 
 } // shredInit()
 
 bool runShred() {
-    // put your setup code here, to run once:
+  // put your setup code here, to run once:
   static unsigned long runTime = 0;
   static unsigned long currentTime = 0;
-  static unsigned long forwardTime = 2000;
+  static unsigned long forwardTime = 1000;
   static unsigned long reverseTime = 7000;
   static unsigned long neutralTime = 1000;
 
@@ -33,62 +35,64 @@ bool runShred() {
 
   static int direction = 1;
 
-  if (digitalRead(SHRED_SAFETY)) { 
+  if (!digitalRead(SHRED_SAFETY)) {
     currentTime = millis();
     if (direction == 1) {
 
-        digitalWrite(SHRED_DIR,HIGH);
-        
-        if (pwm_value < max_speed) {
-          pwm_value = pwm_value +5;
-        }
-        analogWrite(SHRED_PWM,pwm_value);
+      digitalWrite(SHRED_DIR, HIGH);
 
-        // Run shredder motor FORWARD at full speed.
-        if (currentTime - runTime >= forwardTime) {
-          runTime = currentTime;
-          direction = -1;
-        }                    // Send stop command to the motor.
-        
-
-    }else if (direction == -1) {
-        
-        if (pwm_value > 0) {
-          pwm_value = pwm_value - 5;                // Run at full speed
-        }
-
-        analogWrite(SHRED_PWM,pwm_value);     // Set PWM value
-
-        if (currentTime - runTime >= neutralTime) {
-          if (digitalRead(SHRED_DIR) == HIGH) {
-            direction = 0;
-          }else if (digitalRead(SHRED_DIR) == LOW) {
-            direction = 1;
-          }
-          //direction = 1;
-          runTime = currentTime;
-          //Serial.print('n');
-        }
-        
-      }else if (direction == 0) {
-        //Begin reverse direction
-
-        digitalWrite(SHRED_DIR,LOW);    // Set direction to reverse
-        
-        if (pwm_value < max_speed) {
-          pwm_value = pwm_value +5;
-        }
-                        // Run at full speed
-        analogWrite(SHRED_PWM,pwm_value);     // Set PWM value
-
-        if (currentTime - runTime >= reverseTime) {
-          runTime = currentTime;
-          direction = -1;
-        }                    // Send stop command to the motor.
-        //direction = 1;
+      if (pwm_value < max_speed) {
+        pwm_value = pwm_value + 5;
       }
+      analogWrite(SHRED_PWM, pwm_value);
+
+      // Run shredder motor FORWARD at full speed.
+      if (currentTime - runTime >= forwardTime) {
+        runTime = currentTime;
+        direction = -1;
+      } // Send stop command to the motor.
+
+    } else if (direction == -1) {
+
+      if (pwm_value > 0) {
+        pwm_value = pwm_value - 5; // Run at full speed
+      }
+
+      analogWrite(SHRED_PWM, pwm_value); // Set PWM value
+
+      if (currentTime - runTime >= neutralTime) {
+        if (digitalRead(SHRED_DIR) == HIGH) {
+          direction = 0;
+        } else if (digitalRead(SHRED_DIR) == LOW) {
+          direction = 1;
+        }
+        // direction = 1;
+        runTime = currentTime;
+        // Serial.print('n');
+      }
+
+    } else if (direction == 0) {
+      // Begin reverse direction
+
+      digitalWrite(SHRED_DIR, LOW); // Set direction to reverse
+
+      if (pwm_value < max_speed) {
+        pwm_value = pwm_value + 5;
+      }
+      // Run at full speed
+      analogWrite(SHRED_PWM, pwm_value); // Set PWM value
+
+      if (currentTime - runTime >= reverseTime) {
+        runTime = currentTime;
+        direction = -1;
+      } // Send stop command to the motor.
+      // direction = 1;
+    }
+  }
+
+  else {
+    analogWrite(SHRED_PWM, 0);
   }
   return digitalRead(SHRED_SAFETY);
 
-}   // runShred()
-
+} // runShred()
--- include/winder.h
+++ include/winder.h
@@ -1,29 +1,30 @@
 /* winder.h
  * Nikolas Prochaska
  * For ColorSmart SFR
- * Definitions and constants for the operation of 
+ * Definitions and constants for the operation of
  * the winder system
  */
 
 #pragma once
 
+#include "pins.h"
 #include <AccelStepper.h>
 #include <TMCStepper.h>
-#include "pins.h"
 
-#define WIND_R_SENSE     0.11f    // Driver Property
-#define W_MAX_V         100000   // Motor max speed
-#define W_MIN_V         750      // Motor min speed
-#define W_DEF_V         1500     // Motor default speed
-#define W_ACCEL         200000   // Motor Acceleration
+#define WIND_R_SENSE 0.11f // Driver Property
+#define W_MAX_V 100000     // Motor max speed
+#define W_MIN_V 750        // Motor min speed
+#define W_DEF_V 1500       // Motor default speed
+#define W_ACCEL 200000     // Motor Acceleration
 
 // Define Driver with software SPI
-TMC2130Stepper winder(W_CS_PIN, WIND_R_SENSE, W_SW_MOSI, W_SW_MISO, W_SW_SCK); // Software SPI
+TMC2130Stepper winder(W_CS_PIN, WIND_R_SENSE, W_SW_MOSI, W_SW_MISO,
+                      W_SW_SCK); // Software SPI
 using namespace TMC2130_n;
 
 AccelStepper as_winder(AccelStepper::DRIVER, WIND_STEP_PIN, WIND_DIR_PIN);
 
-void winderInit(void){
+void winderInit(void) {
 
   pinMode(W_CS_PIN, OUTPUT);
   pinMode(WIND_EN_PIN, OUTPUT);
@@ -32,17 +33,16 @@ void winderInit(void){
   pinMode(W_SW_MISO, INPUT_PULLUP);
   digitalWrite(WIND_EN_PIN, LOW);
 
-  SPI.begin();
   delay(50);
 
   // TMC driver parameter setup
-  digitalWrite(WIND_EN_PIN,LOW);
+  digitalWrite(WIND_EN_PIN, LOW);
   winder.begin();
   winder.toff(4);
   winder.blank_time(24);
   winder.rms_current(700); // mA
-  //winder.en_pwm_mode(true);      // Enable extremely quiet stepping
-  //winder.pwm_autoscale(true);
+  // winder.en_pwm_mode(true);      // Enable extremely quiet stepping
+  // winder.pwm_autoscale(true);
   winder.microsteps(16);
   winder.TCOOLTHRS(0x0FFFF); // 20bit max
   winder.THIGH(0x00FFF);
@@ -50,19 +50,20 @@ void winderInit(void){
   winder.semax(2);
   winder.sedn(0b01);
 
-
   // AccelStepper init
   as_winder.setMaxSpeed(W_MAX_V);
   as_winder.setAcceleration(W_ACCEL);
 
   as_winder.enableOutputs();
 
-}   // winderInit
+} // winderInit
 
 // Function to run the winder at a set speed
-uint16_t winderRunSpeed(uint16_t w_speed){
-  //Check that input speed is acceptable
-  if((w_speed < W_MIN_V) || (w_speed > W_MAX_V)){ w_speed = W_DEF_V; }
+uint16_t winderRunSpeed(uint16_t w_speed) {
+  // Check that input speed is acceptable
+  if ((w_speed < W_MIN_V) || (w_speed > W_MAX_V)) {
+    w_speed = W_DEF_V;
+  }
 
   // Run the motor at the requested speed
   as_winder.setSpeed(w_speed);
@@ -71,6 +72,4 @@ uint16_t winderRunSpeed(uint16_t w_speed){
   return w_speed; // Return the speed the motor is set to
 } // winderRunSpeed
 
-void winderStop(void){
-  as_winder.stop();
-} // winderStop
+void winderStop(void) { as_winder.stop(); } // winderStop
