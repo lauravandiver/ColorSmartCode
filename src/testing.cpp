05d5d882ffdc5d1a7e8f304893da8ac9684a5c3e
Ndean0103 <nikolaspro31@gmail.com>
2023-04-18 21:32:36 -0700

    Added extrusion capabilities

--- src/testing.cpp
+++ src/testing.cpp
@@ -8,6 +8,7 @@
 #include "shredder.h"
 #include "tempContol.h"
 #include "winder.h"
+#include "extruderMotor.h"
 #include <Arduino.h>
 
 void setup() {
@@ -22,10 +23,11 @@ void setup() {
   pinMode(eboxFan_relay, OUTPUT);
 
   // Initalize subsystems
-  loadcellInit();
+  //loadcellInit();
+  tempInit();
   // indicatorInit();
   alignerInit();
-  digitalWrite(LED_BUILTIN, LOW);
+  // digitalWrite(LED_BUILTIN, LOW);
   hoppersInit();
   pullerInit();
   int fanstat[3] = {1, 1, 1};
@@ -35,7 +37,7 @@ void setup() {
 
   delay(100);
 
-  alignerHome();
+  // alignerHome();
 }
 
 float diameter;            // Filament diameter measurement
@@ -45,6 +47,7 @@ uint16_t pull_speed = 500; // Puller speed to be set
 uint16_t wind_speed = 100; // Winder speed to be set
 Pi2A inputCmd;             // Command structure recieved from Pi
 A2Pi outReport;            // Report structure to be sent to Pi
+bool temp_ready = false;
 
 uint8_t hopper = 0b00000001;
 
@@ -53,22 +56,44 @@ uint32_t tms = millis();
 
 void loop() {
 
-  //Serial.println(micros());
-  runHoppers(hopper);
+  
 
   tms = millis();
-  if(tms-tlast_time > 10000){
-    hopper = hopper<<1;
-    tlast_time = tms;
-    if(hopper > 0b00100000){
-      hopper = 0b00000001;
-    }
-  }
+  // if(tms-tlast_time > 10000){
+  //   hopper = hopper<<1;
+  //   tlast_time = tms;
+  //   if(hopper > 0b00100000){
+  //     hopper = 0b00000001;
+  //   }
+  // }
 
   alignerRun();
   winderRunSpeed(1000);
   pullerRunSpeed(5000);
-  bool safe = runShred();
-  //Serial.println(safe);
-  //  startExtruder(60);
+  // bool safe = runShred();
+
+  if (temp_ready == false) {
+    temp_ready = startExtruder(190);
+    runHoppers(0b00000000);
+    extrudeStop();
+  } else {
+    adjusttemps(190);
+    runHoppers(0b00000000);
+    extrudeRun();
+  }
+
+  //int t1 = thermocouple1.readCelsius();
+
+  if (tms - tlast_time > 1000) {
+    if(temp_ready){Serial.print("A");}
+    Serial.print("Zone 1: ");
+    Serial.print(gettemp1());
+    Serial.print("C;  Zone 2: ");
+    Serial.print(gettemp2());
+    Serial.print("C;  Zone 3: ");
+    Serial.print(gettemp3());
+    Serial.println("C;");
+    Serial.println();
+    tlast_time = tms;
+  }
 }
